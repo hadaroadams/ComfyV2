fdf2fa4e6c010fe11e92d11df38e29dc3ec5c397
hadaroadams <hadaroadams1234@gmail.com>
2023-09-25 08:10:36 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  // const getApi =()=>{
+  //   axios.get('https://www.thecocktaildb.com/api/json/v2/9973533/randomselection.php')
+  //   .then((e)=>{
+  //     console.log(e)
+  //   })
+  //   .catch((e)=>{
+  //     console.log(e)
+  //   })
+  // }
+  // useEffect(()=>{
+  //   getApi()
+  // },[])
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
